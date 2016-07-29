8c7e35dd02db7e32bb290c15d66523d0e3be41f8
uodhayarani <udhayarani806@gmail.com>
2016-07-29 14:10:33 +0530

    oddeven.js
    This program is odd or even function for javascript

---
+++ oddeven.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Even or odd Number</title>
+</head>
+<body>
+<script type="text/javascript">
+   var x=0;
+	if(x%2==0)
+	{
+		document.write("the number is even");
+	}
+	else{
+		document.write("The number is odd");
+	}
+</script>
+</body>
+</html>
