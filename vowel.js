825ad63fdfcbd3eca710d3631d666387485532b5
uodhayarani <udhayarani806@gmail.com>
2016-08-03 14:47:27 +0530

    vowel.js is added

---
+++ vowel.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>vowels and consonent</title>
+</head>
+<body>
+<p id="demo"></p>
+<script type="text/javascript">
+     var x;
+x=prompt("please enter the char");
+if (x=='a' || x=='e' || x=='i' || x=='o' ||x=='u' ||x=='A' ||x=='E' || x=='I' ||x=='O' || x=='U') {
+    document.write("The letter is vowel");
+}
+else
+{
+document.write("The letter is consonant");
+} 
+
+</script>
+
+</body>
+</html>
