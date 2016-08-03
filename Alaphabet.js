9176e71cc0a67ef8b3b4af4dcc29186cd871d8ca
uodhayarani <udhayarani806@gmail.com>
2016-08-03 16:24:45 +0530

    Alphabet.js is added

---
+++ Alaphabet.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Alphabet</title>
+</head>
+<body>
+<script type="text/javascript">
+	var x;
+	x=prompt("please enter the char");
+if(isNaN(x)){
+  document.write("it is an alphabet");
+}
+else {
+  document.write("it is not an alphabet");
+}
+</script>
+</body>
+</html>
