ed3158115c8a741e4632fd61f8779d044edc3fe3
uodhayarani <udhayarani806@gmail.com>
2016-08-12 11:05:32 +0530

    Hello_word.js is added

---
+++ Hello_word.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>For loop</title>
+</head>
+<body>
+<script type="text/javascript">
+	
+	for(x=0; x<5;x++)
+	{
+		document.write("HEllO");
+	}
+</script>
+
+</body>
+</html>
