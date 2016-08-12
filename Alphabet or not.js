ea08bf7a374e672e5b1fc38907be3551d39b06e2
uodhayarani <udhayarani806@gmail.com>
2016-08-12 10:49:46 +0530

    Alphabet or not.js is added

---
+++ Alphabet or not.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Alphabet</title>
+</head>
+<body>
+<script type="text/javascript">
+var c;
+var c=prompt("enter a char");
+
+  if(((c>='a') && (c<= 'z')) || ((c>='A') && (c<='Z'))){
+  	document.write("It is an Alphabet");
+  }
+  else{
+  	document.write("It is not an Alaphabet");
+  }
+  
+   
+
+</script>
+</body>
+</html>
